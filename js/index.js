d9a814013dcefbe583920189ccba0984b9141c9a
iatechdev <Mauricio.roldan@iatech.com.co>
2020-07-23 18:26:22 -0500

    news changes

---
+++ js/index.js
@@ -0,0 +1,71 @@
+function menuresponsive(){
+
+
+    document.getElementById("menures").style.display="block";
+   
+
+}
+
+function displaynone(){
+
+
+    document.getElementById("menures").style.display="none";
+   
+
+}
+
+function showwindow(){
+    document.getElementById("vent").style.display="block";
+    
+}
+function noshow(){
+    document.getElementById("vent").style.display="none";
+
+}
+function staffone(){
+    document.getElementById("staffone").style.display="block";
+    document.getElementById("stafftwo").style.display="none";
+    document.getElementById("staffthree").style.display="none";
+   
+}
+function stafftwo(){
+    document.getElementById("staffone").style.display="none";
+    document.getElementById("stafftwo").style.display="block";
+    document.getElementById("staffthree").style.display="none";
+}
+function staffthree(){
+    document.getElementById("staffone").style.display="none";
+    document.getElementById("stafftwo").style.display="none";
+    document.getElementById("staffthree").style.display="block";
+}
+
+function serviceone(){
+    document.getElementById("resone").style.display="block";
+    document.getElementById("restwo").style.display="none";
+}
+function servicetwo(){
+    document.getElementById("resone").style.display="none";
+    document.getElementById("restwo").style.display="block";
+ 
+}
+/*fab*/
+
+function fabone(){
+    document.getElementById("fabone").style.display="block";
+    document.getElementById("fabtwo").style.display="none";
+    document.getElementById("fabthree").style.display="none";
+ 
+}
+function fabtwo(){
+    document.getElementById("fabone").style.display="none";
+    document.getElementById("fabtwo").style.display="block";
+    document.getElementById("fabthree").style.display="none";
+ 
+}
+function fabthree(){
+    document.getElementById("fabone").style.display="none";
+    document.getElementById("fabtwo").style.display="none";
+    document.getElementById("fabthree").style.display="block";
+ 
+}
+
